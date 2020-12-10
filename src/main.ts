d90c6a22e97e088eb7a62497698161440e06006d
geekduck <duck1218+github@gmail.com>
2020-12-10 09:33:29 +0900

    ElectronをTypeScript化した

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+import { app, BrowserWindow, Menu, Tray } from "electron";
+import * as path from "path";
 
 function createWindow () {
   // Create the browser window.
